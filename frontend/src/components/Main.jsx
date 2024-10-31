26e05bfdfb18a839a020875e1d2cb0c09ca76658
oriana-monaldi <orimonaldi@gmail.com>
2024-10-31 05:22:09 -0300

    altas/main

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import BotonInicio from './BotonInicio'
+import { Link } from 'react-router-dom';
+import BotonInicio from './BotonInicio';
 
 const Main = () => {
     return (
         <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{ backgroundImage: "url('./src/assets/main.jpg')" }} 
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('./src/assets/main.jpg')" }} 
         >
-            <h1 className="text-white text-4xl text-center mt-20 flex items-center justify-center">Compañía Proveedora De Internet</h1>
+        <h1 className="text-white text-4xl text-center mt-20 flex items-center justify-center font-montserrat">
+            Compañía Proveedora De Internet
+        </h1>
+        <div className="flex justify-center mt-4">
             <Link to='/cliente'>
-                <BotonInicio />
+            <BotonInicio />
             </Link>
+        </div>
         </div>
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
-
