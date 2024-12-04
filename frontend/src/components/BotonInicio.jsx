5d741f49240e43a87916e11cdc2b82709cdf7f9f
mateopolci <mateopolci@hotmail.com>
2024-12-04 20:23:39 -0300

    Boton del main arreglado

--- frontend/src/components/BotonInicio.jsx
+++ frontend/src/components/BotonInicio.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function BotonInicio() {
-    const [isHovered, setIsHovered] = useState(false)
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div className='pt-96'>
-            <button
+        <button
             className={`
                 px-10 py-4 rounded-lg text-xl font-bold
                 transition-all duration-300 ease-in-out
-                ${isHovered 
-                ? 'bg-blue-500 text-white shadow-lg shadow-blue-500/50 scale-105' 
-                : 'bg-gray-200 text-gray-700 shadow-md'}
+                ${
+                    isHovered
+                        ? "bg-blue-500 text-white shadow-lg shadow-blue-500/50 scale-105"
+                        : "bg-gray-200 text-gray-700 shadow-md"
+                }
             `}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
-            >Comenzar 
-            </button>
-
-        </div>
-    )
-    }
+        >
+            Comenzar
+        </button>
+    );
+}
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: "url('./src/assets/main.jpg')" }} 
         >
-        <h1 className="text-white text-4xl text-center mt-20 flex items-center justify-center font-montserrat">
+        <h1 className="text-white text-4xl text-center mt-20 flex items-center justify-center font-montserrat mb-96">
             Compañía Proveedora De Internet
         </h1>
         <div className="flex justify-center mt-4">
